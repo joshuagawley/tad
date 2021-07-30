eddd9a275c5e660ad5feaa71c34222662983b857
Joshua Gawley <16921823+a1phabeta@users.noreply.github.com>
2021-07-30 10:33:13 +0100

    Make color-eyre work by installing report handler
    Signed-off-by: Joshua Gawley <16921823+a1phabeta@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
     let args: Vec<String> = std::env::args().skip(1).collect();
     if !(args.len() == 1) {
         Err(eyre::eyre!("Too many arguments specified"))
